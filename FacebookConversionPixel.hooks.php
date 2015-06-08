4a8595b026275f1bf326be3ec84d4f922ce57e09
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2015-06-08 20:51:28 +0300

    FacebookConversionPixel: change JS indentation

--- FacebookConversionPixel.hooks.php
+++ FacebookConversionPixel.hooks.php
@@ -40,16 +40,16 @@ class FacebookConversionPixelHooks {
 
 		$script = <<< SCRIPT
 ‪<script>(function() {
-var _fbq = window._fbq || (window._fbq = []);
-if (!_fbq.loaded) {
-var fbds = document.createElement('script');
-fbds.async = true;
-fbds.src = '//connect.facebook.net/en_US/fbds.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(fbds, s);
-_fbq.loaded = true;
-}
-_fbq.push(['addPixelId', '{$egFacebookConversionPixelId}']);
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+  _fbq.push(['addPixelId', '{$egFacebookConversionPixelId}']);
 })();
 window._fbq = window._fbq || [];
 window._fbq.push(['track', 'PixelInitialized', {}]);
